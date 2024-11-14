c0de4c856494750b5ab9c78a9e765bcfb1728ed0
Amir Salimov <nutado2005@gmail.com>
2024-11-14 22:08:52 +0600

    Complete game

--- game/Menu.js
+++ game/Menu.js
@@ -2,6 +2,7 @@ const readline = require("readline-sync");
 const getRandomDifficulty = require("./Difficulty");
 const wordsAndDifficulty = require("../data/Words");
 const getRandomWord = require("./WordSelector");
+const gallows = require("./HangmanVisual");
 
 let difficultyChoser = function () {
   console.clear();
@@ -30,9 +31,49 @@ let difficultyChoser = function () {
       difficulty = getRandomDifficulty();
       break;
   }
-  let word = getRandomWord(difficulty);
+  word = getRandomWord(difficulty);
   console.log(`Your difficulty: ${difficulty}`);
-  console.log(`Your word: ${word}`);
+  console.log(`Your word: ${word}`); // Отладка-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+  let hiddenWord = word.split("").map(() => "_");
+
+  console.log("\n", hiddenWord, "\n");
+
+  let attemps = 5;
+  let gallowsImages = 0;
+
+  while (hiddenWord.join("") !== word && attemps > 0) {
+    let choseLetter = readline.question("Enter letter: ");
+    let guessingWord = function () {
+      let wordArray = word.split("");
+      let updated = false;
+
+      for (let i = 0; i < wordArray.length; i++) {
+        if (choseLetter === wordArray[i]) {
+          hiddenWord[i] = choseLetter;
+          updated = true;
+        }
+      }
+
+      if (!updated) {
+        attemps--;
+        gallowsImages++;
+        if (gallowsImages < gallows.length) {
+          console.log(gallows[gallowsImages]);
+        }
+      }
+
+      return hiddenWord.join(" ");
+    };
+    guessingWord();
+    console.log(hiddenWord.join(" "));
+  }
+
+  if (hiddenWord.join("") === word) {
+    console.log("You win, word:", word);
+  } else {
+    console.log("You lose, word:", word);
+  }
 };
 
 module.exports = difficultyChoser;
